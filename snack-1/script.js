9820bef978e8efb5ad7c3235ab142523fd917251
GiuntaGiovanni <jhonniest741@gmail.com>
2025-01-21 15:49:41 +0100

    [feat] ex 01 compact

--- snack-1/script.js
+++ snack-1/script.js
@@ -3,8 +3,12 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
 
+// metodo semplice
 names.forEach((name) => {
 console.log(name);  
 })
+
+// metodo compatto
+// names.forEach(name => console.log(name));
 
 
